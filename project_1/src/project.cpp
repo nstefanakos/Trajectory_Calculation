840d737a650a9d8e99d6714d1f15b3bca338f358
nstefanakos <ph07686@uoi.gr>
2020-07-16 18:12:31 +0300

    Changes with bug

--- project_1/src/project.cpp
+++ project_1/src/project.cpp
@@ -68,7 +68,7 @@ void Callback_1(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
 	sensor_msgs::LaserScan new_msg(*msg);
 
-	// map.header.frame_id = "/hokuyo_base_laser_link";
+	map.header.frame_id = "/hokuyo_base_laser_link";
 	map.header.stamp = ros::Time::now();
 
 	if ( count_1 == 0 )
@@ -86,7 +86,7 @@ void Callback_1(const sensor_msgs::LaserScan::ConstPtr& msg)
 		map.info.origin.orientation.w = 0;
 	}
 
-	map.header.frame_id = "Human";
+	// map.header.frame_id = "Human";
 	
 
 	for (int i = 0 ; i < (map.info.width*map.info.height); i++)
@@ -94,16 +94,6 @@ void Callback_1(const sensor_msgs::LaserScan::ConstPtr& msg)
 		map.data.push_back(0);
 	}
 
-	sensor_msgs::LaserScan scan;
-   	scan.angle_min = -45*3.14159/180;
-  	scan.angle_max = 45*3.14159/180;
-  	scan.angle_increment = (scan.angle_max - scan.angle_min) / (720 - 1);
-  	scan.range_max = 10;
-  	scan.header = map.header;
- 	scan.header.frame_id = "laser_frame";
-	ray_caster.laserScanCast(map, scan);
-
-	pub_2.publish(ray_caster);
 
 	if (count_1 == 0)
 	{
@@ -134,6 +124,17 @@ void Callback_1(const sensor_msgs::LaserScan::ConstPtr& msg)
 			new_msg.ranges[i] = 0;
 		}
 	}
+
+	sensor_msgs::LaserScan scan;
+   	scan.angle_min = -90*3.14159/180;
+  	scan.angle_max = 90*3.14159/180;
+  	scan.angle_increment = (scan.angle_max - scan.angle_min) / (720 - 1);
+  	scan.range_max = 10;
+  	scan.header = map.header;
+ 	scan.header.frame_id = "/hokuyo_base_laser_link";
+	ray_caster.laserScanCast(map, scan);
+
+	pub_2.publish(scan);
 
 	int max = 0;
 	int min = new_msg.ranges.size();
@@ -205,11 +206,7 @@ void Callback_1(const sensor_msgs::LaserScan::ConstPtr& msg)
 
 					// float orient_2 = (j==0) ? i*3.14159/180 : k*3.14159/180;
 					float orient_2 = i*3.14159/180;
-					
-					float c = sqrt(pow(old_ranges[i_human], 2.) + pow(vel_param, 2.) - 2*old_ranges[i_human]*vel_param*cos(3.14159 - orient_2));
 
-					float B = acos((pow(old_ranges[i_human], 2.) - pow(vel_param, 2.) + pow(c, 2.))/2*old_ranges[i_human]*c);
-					
 					tf2::Quaternion q;
 		  			q.setRPY(0, 0, orient_2);
 
@@ -219,23 +216,7 @@ void Callback_1(const sensor_msgs::LaserScan::ConstPtr& msg)
 				 	p.orientation.w = q.w();
 
 					// int l = (x - old_x < 0) ? -1 : 1;
-
-				 	int deg = i_human - (B*new_msg.angle_increment);
-
-				 	if (i > 0)
-				 	{
-					 	if (c < old_ranges[i_human + deg])
-					 	{	
-							future.poses.push_back(p);
-					 	}
-				 	}
-				 	else
-				 	{
-				 		if (c < old_ranges[i_human - deg])
-					 	{	
-							future.poses.push_back(p);
-					 	}
-				 	}
+					future.poses.push_back(p);
 				}
 			}
 			pub_1.publish(future);
@@ -261,7 +242,7 @@ int main(int argc, char **argv)
 	ros::Subscriber sub = n.subscribe("/scan", 1000, Callback_1);
 
 	pub_1 = n.advertise<geometry_msgs::PoseArray>("Future_Pose", 1000);
-	pub_2 = n.advertise<map_ray_caster::MapRayCaster>("ray_caster", 1000);
+	pub_2 = n.advertise<sensor_msgs::LaserScan>("ray_caster", 1000);
 
 	ros::spin();
 
